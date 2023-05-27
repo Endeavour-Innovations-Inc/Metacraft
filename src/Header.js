063f7a124795462cfc79a7d0ddb1dfd128b12ebd
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-05-27 12:32:02 -0700

    Applying fixes

--- src/Header.js
+++ src/Header.js
@@ -3,34 +3,27 @@ import Web3 from 'web3';
 
 function Header() {
   const [account, setAccount] = useState('');
+  const web3 = new Web3(window.ethereum);
 
   useEffect(() => {
     if (window.ethereum) {
-      // creating a new Web3 instance
-      const web3 = new Web3(window.ethereum);
       checkIfWalletIsConnected(web3);
     } else {
       alert('Please install MetaMask!');
     }
   }, []);
 
-  const checkIfWalletIsConnected = async (web3) => {
-    // check if we're authorized to access the user's wallet
+  const checkIfWalletIsConnected = async () => {
     const accounts = await web3.eth.getAccounts();
     if (accounts.length !== 0) {
-      // if we have access to the wallet, save the user's account to the state
       setAccount(accounts[0]);
     }
   };
 
   const connectWallet = async () => {
     if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
       try {
-        // request access to the user's wallet
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        // save the user's account to the state
         setAccount(accounts[0]);
       } catch (error) {
         console.error('Failed to connect to MetaMask');
