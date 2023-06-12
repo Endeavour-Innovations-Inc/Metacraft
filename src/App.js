8a5a071c9bcc0d8707b3590bc5adb7e50f603fdb
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-06-12 11:40:19 -0700

    Many changes and fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
-import Header from './Header';
-import Main from './Main';
 
 function App() {
+  const [account, setAccount] = useState('');
+  const [language, setLanguage] = useState('English');
+
+  // Code to switch language
+  const switchLanguage = () => {
+    if (language === 'English') {
+      setLanguage('Russian');
+    } else {
+      setLanguage('English');
+    }
+  };
+
+  useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.getAccounts();
+      if (accounts.length !== 0) {
+        setAccount(accounts[0]);
+      }
+    };
+
+    if (window.ethereum) {
+      checkIfWalletIsConnected();
+    } else {
+      alert('Please install MetaMask!');
+    }
+  }, []);
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.error('Failed to connect to MetaMask');
+      }
+    } else {
+      alert('Please install MetaMask!');
+    }
+  };  
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <div className="header">
+        <button onClick={switchLanguage} className="languageButton">
+          {language === 'English' ? 'РУС' : 'ENG'}
+        </button>
+        {account ? (
+          <p className="accountInfo">Connected with the account: {account}</p>
+        ) : (
+          <button onClick={connectWallet} className="connectButton">Connect to MetaMask</button>
+        )}
+      </div>
+      <div className="main">
+        <h1>Welcome to Our ICO</h1>
+      </div>
     </div>
   );
 }
